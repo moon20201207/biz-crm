61c93b8900413788c1311b52ef9e38ee05bdcfe2
moon <moon20201207@gmail.com>
2025-04-28 21:41:49 +0900

    init

---
+++ src/main/java/com/example/bizcrm/BizCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.bizcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BizCrmApplication.class, args);
+    }
+
+}
